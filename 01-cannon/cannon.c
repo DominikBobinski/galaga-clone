e8a6647252246ba215f3e79ac2f61c9a255e3266
Grzegorz Jabłoński <gwj@dmcs.p.lodz.pl>
2020-02-07 06:46:09 +0100

    Simplified cannon

--- 01-cannon/cannon.c
+++ 01-cannon/cannon.c
@@ -6,11 +6,6 @@ int main(int argc, char *argv[]) {
   if (gfx_init())
     exit(3);
 
-  gfx_filledRect(0, 0, gfx_screenWidth() - 1, gfx_screenHeight() - 1, BLUE);
-  gfx_filledCircle(gfx_screenWidth() / 2, gfx_screenHeight(), 100, YELLOW);
-  gfx_line(gfx_screenWidth() / 2, gfx_screenHeight() , gfx_screenWidth() / 2, gfx_screenHeight() - 150, YELLOW); 
-  gfx_updateScreen();
-
   double angle = 90.0 * (M_PI/180.0);
   while(1)
   {
